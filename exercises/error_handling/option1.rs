cc7761261b58449bbf8fe731e7de55cfefcc4373
Scott McLeod <scotmcle@cisco.com>
2020-01-07 15:51:53 -0500

    Completing option1

--- exercises/error_handling/option1.rs
+++ exercises/error_handling/option1.rs
@@ -4,30 +4,29 @@
 // on `None`. Handle this in a more graceful way than calling `unwrap`!
 // Execute `rustlings hint option1` for hints :)
 
-// I AM NOT DONE
-
 pub fn pop_too_much() -> bool {
     let mut list = vec![3];
 
-    let last: i16 =
-        match list.pop() {
-            None => None,
-            Some(i) => i
-        };
-    // let last = list.pop().unwrap();
 
-    // if let Some(list.pop()) = last_one {
-    //     println!("The last item in the list is {:?}", last_one);
+    // match list.pop() {
+    //     Some(last_one) => println!("The last item in the list is {:?}", last_one),
+    //     None    => (),
     // }
 
-    // if let Some(list.pop()) = second_to_last {
-    //   println!(
-    //     "The second-to-last item in the list is {:?}",
-    //     second_to_last
-    //   );
+    // match list.pop() {
+    //     Some(second_to_last) => println!("The second-to-last item in the list is {:?}", second_to_last),
+    //     None    => (),
     // }
+
+    if let Some(last_one) = list.pop() {
+        println!("The last item in the list is {:?}", last_one);
+    }
+
+    if let Some(second_to_last) = list.pop() {
+        println!("The second-to-last item in the list is {:?}", second_to_last);
+    }
+
     true
-
 }
 
 #[cfg(test)]
